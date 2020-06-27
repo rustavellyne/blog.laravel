90e66fcf756d4360a5b6cad74c9a338fe555085b
Alex Chernyakov <rustavellyne@gmail.com>
2020-06-27 22:58:46 +0300

    before auth saving

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UsersTableSeeder::class);
         $this->call(BookableTableSeeder::class);
         $this->call(BookingTableSeeder::class);
         $this->call(ReviewTableSeeder::class);
